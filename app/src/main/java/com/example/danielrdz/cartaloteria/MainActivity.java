d5b9a39ca691db3c32338b1053b3dedf64b6f3db
aplicaciones3 <dani_181298@hotmail.com>
2019-02-06 13:54:03 -0600

    Carta Mejorada v2

--- app/src/main/java/com/example/danielrdz/cartaloteria/MainActivity.java
+++ app/src/main/java/com/example/danielrdz/cartaloteria/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton carta1,carta2,carta3,carta4,carta5,carta6,carta7,carta8,carta9,carta10,carta11,carta12;
+    int num1=0, num2=0, num3=0, num4=0, num5=0, num6=0, num7=0, num8=0, num9=0, num10=0, num11=0, num12=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,76 +33,162 @@ public class MainActivity extends AppCompatActivity {
         carta1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta1.setImageResource(R.drawable.build);
+                if (num1==0){
+                    carta1.setImageResource(R.drawable.build);
+                    num1=1;
+                }
+                else{
+                    carta1.setImageResource(R.drawable.apache);
+                    num1=0;
+                }
+
             }
         });
 
         carta2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta2.setImageResource(R.drawable.build);
+                if (num2==0){
+                    carta2.setImageResource(R.drawable.build);
+                    num2=1;
+                }
+                else{
+                    carta2.setImageResource(R.drawable.arbol);
+                    num2=0;
+                }
             }
         });
 
         carta3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta3.setImageResource(R.drawable.build);
+                if (num3==0){
+                    carta3.setImageResource(R.drawable.build);
+                    num3=1;
+                }
+                else{
+                    carta3.setImageResource(R.drawable.luna);
+                    num3=0;
+                }
             }
         });
 
         carta4.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta4.setImageResource(R.drawable.build);
+                if (num4==0){
+                    carta4.setImageResource(R.drawable.build);
+                    num4=1;
+                }
+                else{
+                    carta4.setImageResource(R.drawable.arana);
+                    num4=0;
+                }
             }
         });
         carta5.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta5.setImageResource(R.drawable.build);
+                if (num5==0){
+                    carta5.setImageResource(R.drawable.build);
+                    num5=1;
+                }
+                else{
+                    carta5.setImageResource(R.drawable.arpa);
+                    num5=0;
+                }
             }
         });
         carta6.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta6.setImageResource(R.drawable.build);
+                if (num6==0){
+                    carta6.setImageResource(R.drawable.build);
+                    num6=1;
+                }
+                else{
+                    carta6.setImageResource(R.drawable.maceta);
+                    num6=0;
+                }
             }
         });
         carta7.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta7.setImageResource(R.drawable.build);
+                if (num7==0){
+                    carta7.setImageResource(R.drawable.build);
+                    num7=1;
+                }
+                else{
+                    carta7.setImageResource(R.drawable.bandera);
+                    num7=0;
+                }
             }
         });
         carta8.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta8.setImageResource(R.drawable.build);
+                if (num8==0){
+                    carta8.setImageResource(R.drawable.build);
+                    num8=1;
+                }
+                else{
+                    carta8.setImageResource(R.drawable.jaras);
+                    num8=0;
+                }
             }
         });
         carta9.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta9.setImageResource(R.drawable.build);
+                if (num9==0){
+                    carta9.setImageResource(R.drawable.build);
+                    num9=1;
+                }
+                else{
+                    carta9.setImageResource(R.drawable.camaron);
+                    num9=0;
+                }
             }
         });
         carta10.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta10.setImageResource(R.drawable.build);
+                if (num10==0){
+                    carta10.setImageResource(R.drawable.build);
+                    num10=1;
+                }
+                else{
+                    carta10.setImageResource(R.drawable.escalera);
+                    num10=0;
+                }
             }
         });
         carta11.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta11.setImageResource(R.drawable.build);
+                if (num11==0){
+                    carta11.setImageResource(R.drawable.build);
+                    num11=1;
+                }
+                else{
+                    carta11.setImageResource(R.drawable.mano);
+                    num11=0;
+                }
             }
         });
         carta12.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                carta12.setImageResource(R.drawable.build);
+                if (num12==0){
+                    carta12.setImageResource(R.drawable.build);
+                    num12=1;
+
+                }
+                else{
+                    carta12.setImageResource(R.drawable.garza);
+                    num12=0;
+                }
             }
         });
         }
